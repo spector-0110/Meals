1a3ef4edf4e42f3ee53e7e41007bf0d19e337cce
spector-0110 <vatsaadityamishra19@gmail.com>
2023-05-12 12:20:20 +0530

    new project setup

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final theme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      brightness: Brightness.dark,
+      seedColor: const Color.fromARGB(255, 131, 57, 0),
+    ),
+    textTheme: GoogleFonts.latoTextTheme(),
+  );
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      theme: theme,
+      home: Scaffold(),
+    );
+  }
+}
